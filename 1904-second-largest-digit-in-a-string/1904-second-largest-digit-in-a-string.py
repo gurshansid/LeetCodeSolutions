96679799fcb777afdc5e334f40aedf74839c25d6
Gurshan Sidhu <gsidhuseeds@gmail.com>
2025-05-16 15:58:42 +0000

    Time: 10 ms (67.05%), Space: 12.3 MB (98.84%) - LeetHub

--- 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.py
+++ 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.py
@@ -7,7 +7,7 @@ class Solution(object):
                 nums.add(int(s[i]))
         sorted_nums = sorted(nums)
 
-        if (len(sorted_nums) < 2):
+        if (len(sorted_nums) == 1 or len(sorted_nums) == 0):
             return -1
         return sorted_nums[len(sorted_nums) - 2]
 
