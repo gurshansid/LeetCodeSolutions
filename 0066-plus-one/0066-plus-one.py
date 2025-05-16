5c8b7d0cd901a15eb848b1df7d922038b57649c4
Gurshan Sidhu <gsidhuseeds@gmail.com>
2025-05-16 15:02:02 +0000

    Time: 0 ms (100%), Space: 12.5 MB (50.21%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -3,8 +3,7 @@ class Solution(object):
         for i in range(len(digits) - 1, -1, -1):
             if digits[i] < 9:
                 digits[i] += 1
-                return digits  # No carry, done
-            digits[i] = 0  # Set to 0 and carry over
+                return digits  
+            digits[i] = 0  
 
-        # If all digits were 9 (e.g., [9,9,9]), add 1 at the start
         return [1] + digits
