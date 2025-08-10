e8a70d019bcd8a5a07eb9c4761936ba174e7e822
Gurshan Sidhu <gsidhuseeds@gmail.com>
2025-08-09 22:00:42 -0400

    Time: 15 ms (42.39%), Space: 18.5 MB (21.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,3 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        if len(s) != len(t):
-            return False
-        
-        firstString = {}
-        secondString = {}
-
-        for char in s:
-            firstString[char] = firstString.get(char, 0) + 1
-
-        for char in t:
-            secondString[char] = secondString.get(char, 0) + 1
-        
-        return firstString == secondString
-
-        
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
