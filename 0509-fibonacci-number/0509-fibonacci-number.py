0dc5851ecb74edd1b304adc2c61155f460aff44c
Gurshan Sidhu <gsidhuseeds@gmail.com>
2026-07-13 19:42:07 -0400

    Time: 47 ms (63.17%), Space: 19.4 MB (18.25%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = {}
+
+        def dfs(i):
+            if i <= 0:
+                return 0
+            
+            if i == 1:
+                return 1
+
+            
+            if i in dp:
+                return dp[i]
+            
+            dp[i] = dfs(i - 1) + dfs(i - 2)
+            return dp[i]
+        
+        return dfs(n)
